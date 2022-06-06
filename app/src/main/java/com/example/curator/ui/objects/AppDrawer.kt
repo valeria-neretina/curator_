3a3f97711bccda1caf7b3cdb1fed7b5c4b2e113a
valeria-neretina <61146213+valeria-neretina@users.noreply.github.com>
2022-06-06 14:30:16 +0300

    Create AppDrawer.kt

---
+++ app/src/main/java/com/example/curator/ui/objects/AppDrawer.kt
@@ -0,0 +1,126 @@
+package com.example.curator.ui.objects
+
+import android.graphics.drawable.Drawable
+import android.net.Uri
+import android.view.View
+import android.widget.ImageView
+import androidx.drawerlayout.widget.DrawerLayout
+import com.example.curator.R
+import com.example.curator.ui.screens.about.AboutFragment
+import com.example.curator.ui.screens.contacts.CreateChatFragment
+import com.example.curator.ui.screens.groups.AddContactsFragment
+import com.example.curator.ui.settings.SettingsFragment
+import com.example.curator.utilits.APP_ACTIVITY
+import com.example.curator.utilits.USER
+import com.example.curator.utilits.downloadAndSetImage
+import com.example.curator.utilits.replaceFragment
+import com.mikepenz.materialdrawer.AccountHeader
+import com.mikepenz.materialdrawer.AccountHeaderBuilder
+import com.mikepenz.materialdrawer.Drawer
+import com.mikepenz.materialdrawer.DrawerBuilder
+import com.mikepenz.materialdrawer.model.DividerDrawerItem
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem
+import com.mikepenz.materialdrawer.util.AbstractDrawerImageLoader
+import com.mikepenz.materialdrawer.util.DrawerImageLoader
+
+class AppDrawer {
+    private lateinit var mDrawer: Drawer
+    private lateinit var mHeader: AccountHeader
+    private lateinit var mDrawerLayout: DrawerLayout
+    private lateinit var mCurrentProfile:ProfileDrawerItem
+
+    fun create() {
+        /* Создания бокового меню */
+        initLoader()
+        createHeader()
+        createDrawer()
+        mDrawerLayout = mDrawer.drawerLayout}
+
+    fun disableDrawer() {
+        /* Отключение выдвигающего меню */
+        mDrawer.actionBarDrawerToggle?.isDrawerIndicatorEnabled = false
+        APP_ACTIVITY.supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        mDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+        APP_ACTIVITY.mToolbar.setNavigationOnClickListener {
+            APP_ACTIVITY.supportFragmentManager.popBackStack() } }
+
+    fun enableDrawer() {
+        /* Включение выдвигающего меню */
+        APP_ACTIVITY.supportActionBar?.setDisplayHomeAsUpEnabled(false)
+        mDrawer.actionBarDrawerToggle?.isDrawerIndicatorEnabled = true
+        mDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
+        APP_ACTIVITY.mToolbar.setNavigationOnClickListener {
+            mDrawer.openDrawer() } }
+
+    private fun createDrawer() {
+        mDrawer = DrawerBuilder()
+            .withActivity(APP_ACTIVITY)
+            .withToolbar(APP_ACTIVITY.mToolbar)
+            .withActionBarDrawerToggle(true)
+            .withSelectedItem(1)
+            .withAccountHeader(mHeader)
+            .addDrawerItems(
+                PrimaryDrawerItem().withIdentifier(100)
+                    .withIconTintingEnabled(true)
+                    .withName("Создать группу")
+                    .withSelectable(false)
+                    .withIcon(R.drawable.ic_menu_create_groups),
+                PrimaryDrawerItem().withIdentifier(101)
+                    .withIconTintingEnabled(true)
+                    .withName("Создать чат")
+                    .withSelectable(false)
+                    .withIcon(R.drawable.ic_menu_secret_chat),
+                PrimaryDrawerItem().withIdentifier(102)
+                    .withIconTintingEnabled(true)
+                    .withName("Настройки")
+                    .withSelectable(false)
+                    .withIcon(R.drawable.ic_menu_settings),
+                DividerDrawerItem(),
+                PrimaryDrawerItem().withIdentifier(103)
+                    .withIconTintingEnabled(true)
+                    .withName("Вопросы о Curator&Students")
+                    .withSelectable(false)
+                    .withIcon(R.drawable.ic_menu_help)
+            ).withOnDrawerItemClickListener(object : Drawer.OnDrawerItemClickListener {
+                override fun onItemClick(
+                    view: View?,
+                    position: Int,
+                    drawerItem: IDrawerItem<*>
+                ): Boolean {
+                    clickToItem(position)
+                    return false} }).build()}
+
+
+    private fun clickToItem(position:Int){
+        when (position) {
+            1 -> replaceFragment(AddContactsFragment())
+            2 -> replaceFragment(CreateChatFragment())
+            3 -> replaceFragment(SettingsFragment())
+            5-> replaceFragment(AboutFragment())
+        }
+    }
+
+    private fun createHeader() {
+        mCurrentProfile = ProfileDrawerItem()
+            .withName(USER.fullname)
+            .withEmail(USER.phone)
+            .withIcon(USER.photoUrl)
+            .withIdentifier(200)
+        mHeader = AccountHeaderBuilder()
+            .withActivity(APP_ACTIVITY)
+            .withHeaderBackground(R.drawable.header)
+            .addProfiles(
+                mCurrentProfile).build() }
+
+    fun updateHeader(){
+        mCurrentProfile.withName(USER.fullname)
+            .withEmail(USER.phone).withIcon(USER.photoUrl)
+        mHeader.updateProfile(mCurrentProfile) }
+
+    private fun initLoader(){
+        DrawerImageLoader.init(object : AbstractDrawerImageLoader(){
+            override fun set(imageView: ImageView, uri: Uri, placeholder: Drawable) {
+                imageView.downloadAndSetImage(uri.toString()) } }) }
+}
