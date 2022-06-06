e13a8ee51edf01e9a762aadb1b4c63dac6238a37
valeria-neretina <61146213+valeria-neretina@users.noreply.github.com>
2022-06-06 14:26:14 +0300

    Create CommonModel.kt

---
+++ app/src/main/java/com/example/curator/models/CommonModel.kt
@@ -0,0 +1,30 @@
+package com.example.curator.models
+
+/* Общая модель для всех сущностей приложения*/
+
+data class CommonModel(
+    val id: String = "",
+    var username: String = "",
+    var bio: String = "",
+    var fullname: String = "",
+    var state: String = "",
+    var phone: String = "",
+    var photoUrl: String = "empty",
+
+    var text: String = "",
+    var type: String = "",
+    var from: String = "",
+    var timeStamp: Any = "",
+    var fileUrl: String = "empty",
+
+    var lastMessage:String = "",
+    var choice:Boolean = false,
+    var idquestion: String="",
+    var question: String="",
+    var answer: String=""
+
+) {
+    override fun equals(other: Any?): Boolean {
+        return (other as CommonModel).id == id
+    }
+}
