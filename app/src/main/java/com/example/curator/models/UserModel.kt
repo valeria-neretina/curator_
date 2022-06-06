0fd3196c228c8e65ba01496879012bb3e4c1f0ea
valeria-neretina <61146213+valeria-neretina@users.noreply.github.com>
2022-06-06 14:26:24 +0300

    Add files via upload

---
+++ app/src/main/java/com/example/curator/models/UserModel.kt
@@ -0,0 +1,8 @@
+package com.example.curator.models
+
+data class UserModel (
+    val id:String="",
+    var username:String="", var fullname:String="",
+    var state:String="",
+    var photoUrl:String="empty", var bio: String = "",
+    var phone:String="", )
