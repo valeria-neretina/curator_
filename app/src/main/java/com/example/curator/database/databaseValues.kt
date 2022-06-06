84c83b2652119cf23afcc0daee5f3e97763e256e
valeria-neretina <61146213+valeria-neretina@users.noreply.github.com>
2022-06-06 14:24:37 +0300

    Add files via upload

---
+++ app/src/main/java/com/example/curator/database/databaseValues.kt
@@ -0,0 +1,52 @@
+package com.example.curator.utilits
+
+import com.example.curator.models.UserModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.storage.StorageReference
+
+lateinit var AUTH: FirebaseAuth
+lateinit var CURRENT_UID:String
+lateinit var REF_DATABASE_ROOT: DatabaseReference
+lateinit var REF_STORAGE_ROOT:StorageReference
+lateinit var USER:UserModel
+
+const val TYPE_TEXT = "text"
+const val NODE_USERS = "users"
+const val NODE_USERNAMES = "usernames"
+const val NODE_PHONES = "phones"
+const val NODE_PHONES_CONTACTS = "phones_contacts"
+const val NODE_MESSAGES = "messages"
+const val NODE_ABOUT="about"
+
+const val FOLDER_PROFILE_IMAGE = "profile_image"
+const val FOLDER_FILES = "messages_files"
+
+
+const val CHILD_ID = "id"
+const val CHILD_PHONE = "phone"
+const val CHILD_USERNAME = "username"
+const val CHILD_FULLNAME = "fullname"
+const val CHILD_BIO = "bio"
+const val CHILD_PHOTO_URL = "photoUrl"
+const val CHILD_STATE = "state"
+
+const val CHILD_TEXT = "text"
+const val CHILD_TYPE = "type"
+const val CHILD_FROM = "from"
+const val CHILD_TIMESTAMP = "timeStamp"
+const val CHILD_FILE_URL = "fileUrl"
+
+const val NODE_MAIN_LIST = "main_list"
+const val NODE_GROUPS = "groups"
+const val NODE_MEMBERS = "members"
+const val FOLDER_GROUPS_IMAGE = "groups_image"
+const val USER_CREATOR = "creator"
+const val USER_MEMBER = "member"
+
+
+const val USER_ADMIN ="admin"
+const val ABOUT_ID="idquestion"
+const val ABOUT_QUESTION="question"
+const val ABOUT_ANSWER="answer"
+const val FOLDER_MESSAGE_IMAGE = "message_image"
