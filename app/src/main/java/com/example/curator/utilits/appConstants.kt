2ce1751b89e72f9c0a39b40879c42d68758c469d
valeria-neretina <61146213+valeria-neretina@users.noreply.github.com>
2022-06-06 14:45:37 +0300

    Add files via upload

---
+++ app/src/main/java/com/example/curator/utilits/appConstants.kt
@@ -0,0 +1,20 @@
+package com.example.curator.utilits
+
+import com.example.curator.activity.MainActivity
+
+/* Файл содержит все константы приложения*/
+
+lateinit var APP_ACTIVITY:MainActivity
+
+const val TYPE_MESSAGE_IMAGE = "image"
+const val TYPE_MESSAGE_TEXT = "text"
+const val TYPE_MESSAGE_VOICE ="voice"
+const val TYPE_MESSAGE_FILE = "file"
+
+
+const val PICK_FILE_REQUEST_CODE = 301
+
+
+const val TYPE_CHAT ="chat"
+const val TYPE_GROUP ="group"
+const val TYPE_CHANNEL = "channel"
